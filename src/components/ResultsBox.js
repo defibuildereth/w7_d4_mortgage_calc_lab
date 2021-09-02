8e74a6aa6922af1446de7849550b2429d80f4d61
d7o1d1s0 <d7sarmy@gmail.com>
2021-09-02 14:37:28 +0100

    added interest functionality and monthly repayment working

--- src/components/ResultsBox.js
+++ src/components/ResultsBox.js
@@ -4,6 +4,12 @@ const ResultsBox = ({salaries, deposit, duration, interest}) => {
 
     const buyingPower = Number(salaries*3)
 
+    const monthlyInterest = Number(interest/100/12)
+
+    const numberOfMonths = Number(duration*12)
+
+    const monthlyRepayment = Number(buyingPower*(monthlyInterest*(1+monthlyInterest)**numberOfMonths)/((1+monthlyInterest)**numberOfMonths-1))
+
     return (
         <>
         <h4>I'm the Mortgage offer</h4>
@@ -11,7 +17,9 @@ const ResultsBox = ({salaries, deposit, duration, interest}) => {
         <p>Buying power is: £{buyingPower}</p>
         <p>Max House Price: £{buyingPower+Number(deposit)}</p>
         <p>For your desired duration of: {duration} years..</p>
-        <p>The monthly repayment would be: £{buyingPower/(duration*12)}</p>
+        <p>The yearly interest rate is: {interest}</p>
+        <p>The monthly repayment would be: £{monthlyRepayment}</p>
+        <p>The total you repay is: £{monthlyRepayment*numberOfMonths}</p>
         </>
     )
 }
